d644ee5f27d05173f6b21c8ee569dd873c525082
Lode Hoste <zillode@zillode.be>
2012-10-06 00:13:58 +0200

    Import Poker rankings from http://www.dreamincode.net/forums/topic/116864-how-to-make-a-poker-game-in-java/

---
+++ src/edu/vub/at/nfcpoker/Poker.java
@@ -0,0 +1,8 @@
+package edu.vub.at.nfcpoker;
+
+public class Poker {
+	
+	private static String[] suits = { "hearts", "spades", "diamonds", "clubs" };
+	private static String[] ranks  = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	
+}
