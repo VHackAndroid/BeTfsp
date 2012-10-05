cc617b8a72ce559c3df6cd8b1fd6fb3824acbcad
Dries Harnie <dries.harnie@vub.ac.be>
2012-10-05 22:11:01 +0200

    Initial commit

---
+++ src/edu/vub/at/nfcpoker/Splash.java
@@ -0,0 +1,20 @@
+package edu.vub.at.nfcpoker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Splash extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_splash, menu);
+        return true;
+    }
+}
