68723792a2fa87b1cf1a279235a195f3293ab49f
Elisa Gonzalez Boix <egonzale@vub.ac.be>
2012-10-06 18:42:15 +0200

    fixing the chips and bets

--- src/edu/vub/at/nfcpoker/ui/ClientActivity.java
+++ src/edu/vub/at/nfcpoker/ui/ClientActivity.java
@@ -1,43 +1,11 @@
 package edu.vub.at.nfcpoker.ui;
 
 import java.io.IOException;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.tekle.oss.android.animation.AnimationFactory;
-import com.tekle.oss.android.animation.AnimationFactory.FlipDirection;
-
-import mobisocial.nfc.Nfc;
-import mobisocial.nfc.addon.BluetoothConnector;
-import edu.vub.at.commlib.CommLib;
-import edu.vub.at.commlib.CommLibConnectionInfo;
-import edu.vub.at.nfcpoker.Card;
-import edu.vub.at.nfcpoker.ConcretePokerServer.GameState;
-import edu.vub.at.nfcpoker.R;
-import edu.vub.at.nfcpoker.ui.tools.PageProvider;
-import fi.harism.curl.CurlPage;
-import fi.harism.curl.CurlView;
-import edu.vub.at.nfcpoker.comm.Message.ReceiveHoleCardsMessage;
-import edu.vub.at.nfcpoker.comm.Message.ReceivePublicCards;
-import edu.vub.at.nfcpoker.comm.PokerServer;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -47,22 +15,28 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnLayoutChangeListener;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ViewFlipper;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.google.common.base.CaseFormat;
+
+import edu.vub.at.commlib.CommLib;
+import edu.vub.at.commlib.CommLibConnectionInfo;
+import edu.vub.at.nfcpoker.Card;
+import edu.vub.at.nfcpoker.ConcretePokerServer.GameState;
+import edu.vub.at.nfcpoker.R;
+import edu.vub.at.nfcpoker.comm.Message.ReceiveHoleCardsMessage;
+import edu.vub.at.nfcpoker.comm.Message.ReceivePublicCards;
+import edu.vub.at.nfcpoker.comm.PokerServer;
+import edu.vub.at.nfcpoker.ui.tools.PageProvider;
+import fi.harism.curl.CurlView;
 
 public class ClientActivity extends Activity implements OnClickListener{
 
@@ -70,6 +44,7 @@ public class ClientActivity extends Activity implements OnClickListener{
 	//public static GameState GAME_STATE = GameState.INIT;
 	private static int currentBet = 0;
 	private static int currentMoney = 0;
+	private int currentChipSwiped = 0;
 	
 	// UI
 	private CurlView mCardView1;
@@ -124,44 +99,23 @@ public class ClientActivity extends Activity implements OnClickListener{
         /*final Intent intent = new Intent(NfcAdapter.ACTION_TAG_DISCOVERED);
         intent.putExtra(NfcAdapter.EXTRA_NDEF_MESSAGES, AsciiNdefMessage.CreateNdefMessage(UUID));
         startActivity(intent);*/
-        final ImageButton buttonAddBlack = (ImageButton) findViewById(R.id.AddBlack);
-        buttonAddBlack.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (canBet()) incrementBetAmount(100);
-            }
-        });
-        final ImageButton buttonAddGreen = (ImageButton) findViewById(R.id.AddGreen);
-        buttonAddGreen.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (canBet()) incrementBetAmount(25);
-            }
-        });
-        final ImageButton buttonAddBlue = (ImageButton) findViewById(R.id.AddBlue);
-        buttonAddBlue.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (canBet()) incrementBetAmount(10);
-            }
-        });
-        final ImageButton buttonAddRed = (ImageButton) findViewById(R.id.AddRed);
-        buttonAddRed.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (canBet()) incrementBetAmount(5);
-            }
-        });
-        final ImageButton buttonAddWhite = (ImageButton) findViewById(R.id.AddWhite);
-        buttonAddWhite.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (canBet()) incrementBetAmount(1);
-            }
-        });
         
         // Gesture detection
         gestureDetector = new GestureDetector(this, new MyGestureDetector());
         gestureListener = new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(View arg0, MotionEvent arg1) {
-				return gestureDetector.onTouchEvent(arg1);
-			}
+        	@Override
+        	public boolean onTouch(View arg0, MotionEvent arg1) {
+        		int viewSwiped = arg0.getId();
+        		switch(viewSwiped) {
+        			case R.id.whitechip: currentChipSwiped = 5; break;
+        				case R.id.redchip: currentChipSwiped = 10; break;
+        				case R.id.greenchip: currentChipSwiped = 20; break;
+        				case R.id.bluechip: currentChipSwiped = 50; break;
+        				case R.id.blackchip: currentChipSwiped = 100; break;
+        				default: Log.v("AMBIENTPOKER", "wrong view swipped" + viewSwiped);
+        		}
+        		return gestureDetector.onTouchEvent(arg1);
+        	}
         };
         
         final ImageView whitechip = (ImageView) findViewById(R.id.whitechip);
@@ -210,6 +164,7 @@ public class ClientActivity extends Activity implements OnClickListener{
         mCardView2.setCurrentIndex(0);
         mCardView2.setBackgroundColor(POKER_GREEN);        
         
+        incrementBetAmount(0);
         listenToGameServer();
     }
     
@@ -291,7 +246,7 @@ public class ClientActivity extends Activity implements OnClickListener{
     }
     
     private void updateHandGui(ReceiveHoleCardsMessage cards) {
-
+ 
         int id1 = getResources().getIdentifier("edu.vub.at.nfcpoker:drawable/" + cards.card1.toString(), null, null);
         int[] bitmapIds1 = new int[] { R.drawable.backside, id1 };
         mCardView1.setPageProvider(new PageProvider(this, bitmapIds1));
@@ -356,8 +311,8 @@ public class ClientActivity extends Activity implements OnClickListener{
     // Game
     private void incrementBetAmount(int value) {
     	currentBet += value;
-        final EditText textCurrentBet = (EditText) findViewById(R.id.currentBet);
-        textCurrentBet.setText(""+currentBet);
+        final TextView textCurrentBet = (TextView) findViewById(R.id.currentBet);
+        textCurrentBet.setText(" " + currentBet);
     }
     
     private boolean canViewCards() {
@@ -466,13 +421,19 @@ public class ClientActivity extends Activity implements OnClickListener{
          @Override
          public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
              try {
-                 if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                 if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH)
                      return false;
                  // right to left swipe
-                 if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                     Toast.makeText(ClientActivity.this, "Left Swipe", Toast.LENGTH_SHORT).show();
-                 }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                     Toast.makeText(ClientActivity.this, "Right Swipe", Toast.LENGTH_SHORT).show();
+                 if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                	 if (canBet()) { 
+                		 Toast.makeText(ClientActivity.this, "Adding to the bet " + currentChipSwiped, Toast.LENGTH_SHORT).show();
+                		 incrementBetAmount(currentChipSwiped);
+                	 }
+                 }  else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                	 if (canBet()) { 
+                	 Toast.makeText(ClientActivity.this, "Removing to the bet " + currentChipSwiped, Toast.LENGTH_SHORT).show();
+                	 incrementBetAmount(-currentChipSwiped);
+                	 }
                  }
              } catch (Exception e) {
                  // nothing
