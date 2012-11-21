e1e526734c180d1a1101778f31c2b51919b67316
Dries Harnie <dries.harnie@vub.ac.be>
2012-11-21 10:52:08 +0100

    Use shorter base URL for QR codes

--- src/edu/vub/at/nfcpoker/Constants.java
+++ src/edu/vub/at/nfcpoker/Constants.java
@@ -2,7 +2,7 @@ package edu.vub.at.nfcpoker;
 
 public class Constants {
 	// Constants for the "join wePoker" intent.
-	public static final String INTENT_BASE_URL       = "https://code.google.com/p/ambienttalk/wiki/WePoker/play/";
+	public static final String INTENT_BASE_URL       = "http://wepoker.info/play/";
 	public static final String INTENT_SERVER_IP      = "i";
 	public static final String INTENT_PORT           = "o";
 	public static final String INTENT_WIFI_PASSWORD  = "p";
