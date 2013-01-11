7cdd871747d73634dd0abffbec9562475c196e22
Lode Hoste <zillode@zillode.be>
2013-01-11 08:01:15 +0100

    Added discovery changed action broadcast intent

--- src/edu/vub/at/nfcpoker/ui/WifiDirectManager.java
+++ src/edu/vub/at/nfcpoker/ui/WifiDirectManager.java
@@ -116,9 +116,10 @@ public class WifiDirectManager extends BroadcastReceiver implements GroupInfoLis
 			Log.d("wePoker - Wifi-Direct", "Initialising manager");
 			this.channel = manager.initialize(act, l, null);
 			mWifiDirectIntentFilter = new IntentFilter();
-		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
 		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_DISCOVERY_CHANGED_ACTION);
+		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
 		    mWifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 		}
 	}
